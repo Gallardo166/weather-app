2a955a15e2afebedc0066ea9639275a7a9cc45b1
Gallardo Lee <leegallardo16@gmail.com>
2024-03-25 21:15:37 +0700

    Enable toggle temp units

--- src/weather-page.js
+++ src/weather-page.js
@@ -4,6 +4,7 @@ const init = function() {
 
   const input = document.getElementById('city-name');
   const searchButton = document.getElementById('search-button');
+  const tempUnitButton = document.getElementById('temperature-unit-button');
   const cityName = document.getElementById('city');
   const temperature = document.getElementById('temperature');
   const feelsLike = document.getElementById('feels-like');
@@ -16,15 +17,35 @@ const init = function() {
     searchButton.addEventListener('click', (event) => {
       getWeatherInfo(input.value)
         .then(function(resolve) {
-          displayWeatherInfo(resolve);
+          displayWeatherInfo(resolve, tempUnitButton.dataset.unit);
+          searchButton.setAttribute('data-weather-info', JSON.stringify(resolve));
       });
       input.value = '';
       event.preventDefault();
     })
 
+    tempUnitButton.addEventListener('click', (event) => {
+      if (tempUnitButton.getAttribute('data-unit') === 'celsius') {
+        tempUnitButton.setAttribute('data-unit', 'fahrenheit');
+        tempUnitButton.textContent = '°F';
+      } else {
+        tempUnitButton.setAttribute('data-unit', 'celsius');
+        tempUnitButton.textContent = '°C';
+      }
+      if (searchButton.dataset.weatherInfo !== '') {displayWeatherInfo(JSON.parse(searchButton.dataset.weatherInfo), tempUnitButton.dataset.unit)};
+      event.preventDefault();
+    })
   }
 
-  const displayWeatherInfo = function(data) {
+  const displayWeatherInfo = function(data, unit) {
+    if (unit === 'celsius') {
+      displayWeatherInfoInCelsius(data);
+    } else {
+      displayWeatherInfoInFahrenheit(data);
+    };
+  }
+
+  const displayWeatherInfoInCelsius = function(data) {
     cityName.textContent = `${data.cityName}, ${data.countryName}`;
     temperature.textContent = data.celsiusTemp;
     feelsLike.textContent = `Feels like: ${data.celsiusFeelsLike}`;
@@ -32,6 +53,15 @@ const init = function() {
     humidity.textContent = `Humidity: ${data.humidity}%`;
     condition.textContent = data.condition;
 
+  }
+
+  const displayWeatherInfoInFahrenheit = function(data) {
+    cityName.textContent = `${data.cityName}, ${data.countryName}`;
+    temperature.textContent = data.fahrenheitTemp;
+    feelsLike.textContent = `Feels like: ${data.fahrenheitFeelsLike}`;
+    windSpeed.textContent = `Wind: ${data.windSpeed} kph`;
+    humidity.textContent = `Humidity: ${data.humidity}%`;
+    condition.textContent = data.condition;
   }
 
   addEvents();
